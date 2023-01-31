5c5d8f69f1a22c9f517576474e02071f4d0bae9f
hungpham <phamhung20022015@gmail.com>
2023-01-31 10:25:37 +0700

    feat(ai-web): Include STATIC_ROOT, STATICFILES_DIRS to load static file for production and development phrase respectively

--- ai_web/settings.py
+++ ai_web/settings.py
@@ -127,8 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "productionfiles/"
+
 STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
