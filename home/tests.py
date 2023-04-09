1055db0754ee03da46d95f02c0096b133a433e3c
Hung Pham <phamhung20022015@gmail.com>
2023-04-09 14:05:08 +0700

    feat(home): add units test for login page connection

--- home/tests.py
+++ home/tests.py
@@ -4,11 +4,16 @@ from django.urls import reverse
 
 class HomepageTests(SimpleTestCase):
     def setUp(self):
-        url = reverse("home:index")
-        self.response = self.client.get(url)
+        home_url = reverse("home:index")
+        login_url = reverse("home:login")
+        self.home_response = self.client.get(home_url)
+        self.login_response = self.client.get(login_url)
 
     def test_homepage_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.home_response.status_code, 200)
 
     def test_homepage_template(self):
-        self.assertTemplateUsed(self.response, "home/index.html")
+        self.assertTemplateUsed(self.home_response, "home/index.html")
+
+    def test_loginpage_status_code(self):
+        self.assertEqual(self.login_response.status_code, 200)
